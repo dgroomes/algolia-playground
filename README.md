c0ec9c944091617e2c562f671b9e7fe195937aac
David Groomes <dwgroomes@gmail.com>
2022-07-14 19:23:57 -0500

    📚 Learning and exploring the Algolia search platform

---
+++ README.md
@@ -0,0 +1,30 @@
+# algolia-playground
+
+📚 Learning and exploring the Algolia search platform.
+
+> The best Search and Discovery Platform for your business
+>
+> -- <cite>https://www.algolia.com/</cite>
+
+
+## Description
+
+This project was scaffolded with Algolia's official tool: [create-instantsearch-app](https://github.com/algolia/create-instantsearch-app)
+and then I adapted it to my typical style.
+
+
+## Instructions
+
+Follow these instructions to build and serve a web page that hosts Algolia search controls:
+
+1. Install dependencies:
+   * ```shell
+     npm install
+     ```
+1. Build and serve the page continuously with the development server:
+   * ```shell
+     npm start
+     ```
+1. Open the page and try it out
+   * Open the browser to <http://localhost:1234>.
+   * Search for something, like "Node.js". The page is making requests to the Algolia servers to execute the search. 
---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import algoliasearch from 'algoliasearch/lite';
+import {Configure, Highlight, Hits, InstantSearch, Pagination, SearchBox,} from 'react-instantsearch-hooks-web';
+
+import type {Hit} from 'instantsearch.js';
+
+import './App.css';
+
+// Algolia app IDs and API keys are not exactly secrets because they are used client-side where anyone can see them. But
+// I might still consider omitting them from version control.
+const MY_APP_ID = '';
+const MY_API_KEY = '';
+
+const searchClient = algoliasearch(MY_APP_ID, MY_API_KEY);
+
+export function App() {
+    return (
+        <div className="container">
+            <InstantSearch searchClient={searchClient} indexName="sample_stackoverflow_posts">
+                <Configure hitsPerPage={8}/>
+                <div className="search-panel">
+                    <div className="search-panel__results">
+                        <SearchBox placeholder="" className="searchbox"/>
+                        <Hits hitComponent={Hit}/>
+
+                        <div className="pagination">
+                            <Pagination/>
+                        </div>
+                    </div>
+                </div>
+            </InstantSearch>
+        </div>
+    );
+}
+
+type HitProps = {
+    hit: Hit;
+};
+
+function Hit({hit}: HitProps) {
+    return (
+        <article>
+            <h1>
+                <Highlight attribute="post_title" hit={hit}/>
+            </h1>
+            <p>
+                <Highlight attribute="author_name" hit={hit}/>
+            </p>
+            <p>
+                <Highlight attribute="content" hit={hit}/>
+            </p>
+        </article>
+    );
+}
